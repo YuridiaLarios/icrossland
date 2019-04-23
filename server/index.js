180d96d56eb6adf682facd1842f9795bd0c0414e
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-04-23 10:29:17 -0700

    add path to serve the index.html from expr server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.use(express.json()); // get ability to use body for post, put, delete;
 
+
 const {
   Pool
 } = require('pg')
@@ -11,12 +12,19 @@ const pool = new Pool({
   ssl: process.env.NODE_ENV === 'production'
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 pool.connect();
 
-app.get('/', (req, res) => {
-  res.send('hello testing root');
-});
 
 app.get('/users', async (req, res) => {
   const client = await pool.connect();
