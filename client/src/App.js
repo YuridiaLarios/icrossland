b81e52dee2e919a9100dd13095b4d34d0fa64529
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-04-24 10:24:00 -0700

    add BrowserRouters and test pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button, Jumbotron } from 'react-bootstrap';
+// import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {NavigationBar} from './components/NavigationBar';
+import {Jumbotron} from './components/Jumbotron';
+import {Layout} from './components/Layout';
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
+import { NoMatch } from './NoMatch';
 
 
 function App() {
   return (
-    <div className="App">
-      <Jumbotron>
-        <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>;
-    </div>
+    <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Jumbotron />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
+        </Router>
+      </React.Fragment>
   );
 }
 
---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Button, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .jumbo {
+    text-align: center;
+  }
+`;
+
+
+export const Jumbotron = () => (
+  <Styles>
+    <Jumbo className='jumbo'>
+      <Container>
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Container>
+    </Jumbo>
+  </Styles>
+)
