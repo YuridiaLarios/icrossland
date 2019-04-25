9528e5ca3f31f14de27a95ab58fc5e72033ff8d4
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-04-25 14:47:32 -0700

    require('dotenv').config()

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+
+
 // const User = require('../models/user-model');
+
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -18,8 +22,8 @@ passport.deserializeUser((id, done) => {
 passport.use(
     new GoogleStrategy({
         // options for google strategy
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
+        clientID: process.env.DB_GOOGLEID,
+        clientSecret: process.env.DB_CLIENTSECRET,
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // check if user already exists in our own db
