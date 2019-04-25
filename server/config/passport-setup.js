38817032bf4e5902073aa0ad9cbbdc155254bad5
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-04-25 15:15:01 -0700

    delete keys

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./keys');
+//const keys = require('./keys');
 
 
 // const User = require('../models/user-model');
