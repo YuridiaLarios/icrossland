ee318d2767e9a95bcf9321f52351973ad55fead6
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-05-04 23:35:39 -0700

    organize user routes on server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 // setup environment
 dotenv.config();
 const app = express();
-const path = require('path');
-const User = require('./models/user-model');
-const cors = require('cors');
-const morgan = require('morgan');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+const path = require("path");
+const User = require("./models/user-model");
+const cors = require("cors");
+const morgan = require("morgan");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const users = require("./routes/users");
 
 app.use(cors());
 
@@ -20,11 +21,12 @@ app.use(morgan(`API Request (port ${PORT}): :method :url :status :response-time 
 
 // get ability to use bodyParser for post, put, delete;
 app.use(express.json()); 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
+app.use("/api", users);
 
 // mongo db connect
-mongoose.set('useCreateIndex', true)
+mongoose.set("useCreateIndex", true)
 mongoose.connect(process.env.DB_MONGODBURI, {
   useNewUrlParser: true
 }, function (error) {
@@ -33,85 +35,6 @@ mongoose.connect(process.env.DB_MONGODBURI, {
   } else {
     console.log("connected to mongo database yay!")
   }
-});
-
-
-/*************************************
- ROUTES FOR TESTING PURPOSES
-**************************************/
-app.get('/api/public', function (req, res) {
-  res.json({
-    message: "Hello from a public endpoint! You don't need to be authenticated to see this."
-  });
-});
-
-app.get('/api/private', function (req, res) {
-  res.json({
-    message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
-  });
-});
-
-
-/*************************************
-ACTUAL ROUTES
-**************************************/
-app.get('/api/allusers', function(req, res) {
-  User.find({}, function(error, users){
-    if(error){
-      console.log("problem finding data");
-      res.send("something went really wrong!!!");
-      next();
-    } 
-    // console.log(users);
-    res.json(users);
-  });
-})
-
-app.post('/api/user', function(req,res) {
-  User.findOne({authId: req.body.sub}).then((currentUser) => {
-    if (currentUser) {
-      // already have user
-      console.log('User is ', currentUser);
-    } else {
-      // if not, create user in our db
-      User.create({
-        authId: req.body.sub,
-        username: req.body.name,
-        email: req.body.email,
-        thumbnailFile: req.body.picture
-      }, function(error, data){
-        if(error){
-          console.log("There was a problem adding a document to the collection.");
-          console.log(error);
-          res.sendStatus(500)
-        } else {
-          console.log("Data added to collection: ");
-          console.log(data);
-          res.sendStatus(200)
-        }
-      })
-    }
-  })
-});
-
-
-const {
-  Pool
-} = require('pg')
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/icrossland_db',
-  ssl: process.env.NODE_ENV === 'production'
-})
-
-
-pool.connect();
-
-
-app.get('/users', async (req, res) => {
-  const client = await pool.connect();
-  var users = await client.query("SELECT * FROM users");
-  res.json(users.rows);
-  client.release();
 });
 
 
@@ -127,7 +50,7 @@ const checkJwt = jwt({
   // Validate the audience and the issuer.
   audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
+  algorithms: ["RS256"]
 });
 
 
---
+++ server/routes/users.js
@@ -0,0 +1,73 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const User = require("../models/user-model");
+
+
+
+
+/*************************************
+ ROUTES FOR TESTING PURPOSES
+**************************************/
+
+router.route("/users").get((req,res) => {
+  res.send("Hello you!");
+})
+
+router.get("/public", function (req, res) {
+  res.json({
+    message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+  });
+});
+
+router.get("/private", function (req, res) {
+  res.json({
+    message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+  });
+});
+
+
+
+/*************************************
+ACTUAL ROUTES
+**************************************/
+router.get("/allusers", function(req, res) {
+  User.find({}, function(error, users){
+    if(error){
+      console.log("problem finding data");
+      res.send("something went really wrong!!!");
+      next();
+    } 
+    // console.log(users);
+    res.json(users);
+  });
+})
+
+router.post("/user", function(req,res) {
+  User.findOne({authId: req.body.sub}).then((currentUser) => {
+    if (currentUser) {
+      // already have user
+      console.log("User is ", currentUser);
+    } else {
+      // if not, create user in our db
+      User.create({
+        authId: req.body.sub,
+        username: req.body.name,
+        email: req.body.email,
+        thumbnailFile: req.body.picture
+      }, function(error, data){
+        if(error){
+          console.log("There was a problem adding a document to the collection.");
+          console.log(error);
+          res.sendStatus(500)
+        } else {
+          console.log("Data added to collection: ");
+          console.log(data);
+          res.sendStatus(200)
+        }
+      })
+    }
+  })
+});
+
+
+module.exports = router;
